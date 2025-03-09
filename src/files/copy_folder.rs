9f0d004db0da6e178a577b571b5c47d865172486
Javier Arrieta <javier@techdelivery.es>
2025-03-09 20:50:54 +0000

    fmt

--- src/files/copy_folder.rs
+++ src/files/copy_folder.rs
@@ -1,13 +1,11 @@
-
-
+use crate::common::delegate::UploadDelegateConfig;
+use crate::common::drive_file;
+use crate::common::hub_helper;
+use crate::files;
 use crate::files::copy;
 use crate::files::list;
 use crate::files::mkdir;
 use crate::hub::Hub;
-use crate::common::drive_file;
-use crate::common::hub_helper;
-use crate::files;
-use crate::common::delegate::UploadDelegateConfig;
 use std::error;
 use std::fmt::Display;
 use std::fmt::Formatter;
@@ -26,22 +24,22 @@ pub async fn copy_folder(config: Config) -> Result<(), Error> {
         .await
         .map_err(Error::GetFile)?;
 
-        err_if_not_directory(&src_folder)?;
+    err_if_not_directory(&src_folder)?;
 
-        let to_parent = files::info::get_file(&hub, &config.to_folder_id)
-            .await
-            .map_err(Error::GetDestinationFolder)?;
+    let to_parent = files::info::get_file(&hub, &config.to_folder_id)
+        .await
+        .map_err(Error::GetDestinationFolder)?;
 
-        err_if_not_directory(&to_parent)?;
+    err_if_not_directory(&to_parent)?;
 
-        let copy_config = CopyFolderConfig {
-            src_folder_id: config.src_folder_id,
-            to_folder_id: config.to_folder_id,
-        };
+    let copy_config = CopyFolderConfig {
+        src_folder_id: config.src_folder_id,
+        to_folder_id: config.to_folder_id,
+    };
 
-        copy_folder_inner(&hub, delegate_config, &copy_config).await?;
+    copy_folder_inner(&hub, delegate_config, &copy_config).await?;
 
-        Ok(())
+    Ok(())
 }
 
 pub struct CopyFolderConfig {
@@ -52,16 +50,18 @@ pub struct CopyFolderConfig {
 pub async fn copy_folder_inner(
     hub: &Hub,
     delegate_config: UploadDelegateConfig,
-    config: &CopyFolderConfig
+    config: &CopyFolderConfig,
 ) -> Result<(), Error> {
-
     let list_children_config: list::ListFilesConfig = list::ListFilesConfig {
-        query: list::ListQuery::FilesInFolder { folder_id: config.to_folder_id.clone() },
+        query: list::ListQuery::FilesInFolder {
+            folder_id: config.to_folder_id.clone(),
+        },
         order_by: list::ListSortOrder::FolderModifiedName,
         max_files: 1000,
     };
 
-    let children = list::list_files(hub, &list_children_config).await
+    let children = list::list_files(hub, &list_children_config)
+        .await
         .map_err(Error::ListFiles)?;
 
     for child in children {
@@ -72,24 +72,31 @@ pub async fn copy_folder_inner(
                 parents: Some(vec![config.to_folder_id.clone()]),
                 print_only_id: true,
             };
-            let new_folder = mkdir::create_directory(hub,&mkdir_config, delegate_config.clone() ).await.map_err(Error::MKDirError)?;
-            let new_folder_config = 
-                CopyFolderConfig { 
-                    src_folder_id: child.id.ok_or(Error::FileWithoutId)?,
-                    to_folder_id: new_folder.id.ok_or(Error::FileWithoutId)?
-                };
-            Box::pin(copy_folder_inner(hub, delegate_config.clone(), &new_folder_config)).await?;
+            let new_folder = mkdir::create_directory(hub, &mkdir_config, delegate_config.clone())
+                .await
+                .map_err(Error::MKDirError)?;
+            let new_folder_config = CopyFolderConfig {
+                src_folder_id: child.id.ok_or(Error::FileWithoutId)?,
+                to_folder_id: new_folder.id.ok_or(Error::FileWithoutId)?,
+            };
+            Box::pin(copy_folder_inner(
+                hub,
+                delegate_config.clone(),
+                &new_folder_config,
+            ))
+            .await?;
         } else {
             let copy_config = copy::CopyConfig {
                 file_id: child.id.ok_or(Error::FileWithoutId)?,
                 to_folder_id: config.to_folder_id.clone(),
             };
-            copy::copy_file(hub, delegate_config.clone(), &copy_config).await.map_err(Error::Copy)?;
+            copy::copy_file(hub, delegate_config.clone(), &copy_config)
+                .await
+                .map_err(Error::Copy)?;
         }
     }
 
     Ok(())
-
 }
 
 impl Display for Error {
